c189ac5a3872654a5f4b897837f4f1830052864c
Thanh <nmthanh20006@gmail.com>
2022-01-08 01:34:12 +0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+/* @remove-on-es-build-begin */
+// this file is not used if use https://github.com/ant-design/babel-plugin-import
+
+/* @remove-on-es-build-end */
+
+export { default as Col } from './components/grid/col';
+
+export { default as Grid } from './components/grid';
+
+export { default as Row } from './components/grid/row';
